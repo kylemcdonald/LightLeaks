018e19cb159983c27ffd553608d42713ea023da4
Kyle McDonald <kyle@kylemcdonald.net>
2013-05-08 15:26:43 +0200

    structured light capture via osc

---
+++ EdsdkOsc/src/testApp.cpp
@@ -0,0 +1,60 @@
+#include "testApp.h"
+
+void testApp::setup() {
+	ofSetVerticalSync(true);
+	ofSetFrameRate(120);
+	ofHideCursor();
+	ofSetLogLevel(OF_LOG_VERBOSE);
+	oscIn.setup(9000);
+	oscOut.setup("disco.local", 9001);
+	capturing = false;
+}
+
+void testApp::update() {
+	ofxOscMessage msgIn;
+	while(oscIn.getNextMessage(&msgIn)) {
+		if(msgIn.getAddress() == "/takePhoto") {
+			camera.takePhoto();
+			capturing = true;
+		}
+		if(msgIn.getAddress() == "/savePhoto") {
+			string filename = msgIn.getArgAsString(0);
+			camera.savePhoto(filename);
+		}
+		if(msgIn.getAddress() == "/update") {
+			camera.update();
+		}
+		if(msgIn.getAddress() == "/setup") {
+			camera.setup();
+		}
+		if(msgIn.getAddress() == "/createDirectory") {
+			string directory = msgIn.getArgAsString(0);
+			ofDirectory::createDirectory(directory, true, true);
+		}
+	}
+	
+	if(camera.isPhotoNew()) {
+		ofxOscMessage msgOut;
+		msgOut.setAddress("/newPhoto");
+		oscOut.sendMessage(msgOut);
+	}
+	
+	ofxOscMessage msgOut;
+	msgOut.setAddress("/capturing");
+	if(!capturing) {
+		msgOut.addIntArg(0);
+	} else {
+		msgOut.addIntArg(1);
+	}
+	oscOut.sendMessage(msgOut);
+}
+
+void testApp::draw() {
+	ofBackground(0);
+	ofSetColor(255);
+	if(!capturing) {
+		camera.draw(0, 0);
+	}
+	ofDrawBitmapStringHighlight("savePath: " + savePath, 10, 20);
+	ofDrawBitmapStringHighlight("capturing: " + ofToString(capturing), 10, 40);
+}
---
+++ ProCamSampleEdsdkOsc/src/testApp.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include "ofMain.h"
+
+#include "ofxOsc.h"
+
+class EdsdkOsc {
+protected:
+	ofxOscReceiver oscIn;
+	ofxOscSender oscOut;
+	bool newPhoto;
+	int width, height;
+	
+	void sendMessage(string address) {
+		ofxOscMessage msgOut;
+		msgOut.setAddress(address);
+		oscOut.sendMessage(msgOut);
+	}
+	
+public:
+	EdsdkOsc()
+	:newPhoto(false),
+	width(1056),
+	height(704) {
+	}
+	void setup() {
+		oscIn.setup(9001);
+		oscOut.setup("kyle.local", 9000);
+		ofAddListener(ofEvents().update, this, &EdsdkOsc::updateOsc);
+		sendMessage("/setup");
+	}
+	void updateOsc(ofEventArgs &args) {
+		ofxOscMessage msgIn;
+		while(oscIn.getNextMessage(&msgIn)) {
+			if(msgIn.getAddress() == "/newPhoto") {
+				newPhoto = true;
+			}
+		}
+	}
+	void createDirectory(string directory) {
+		ofxOscMessage msgOut;
+		msgOut.setAddress("/createDirectory");
+		msgOut.addStringArg(directory);
+		oscOut.sendMessage(msgOut);
+	}
+	void update() {
+		sendMessage("/update");
+	}
+	void takePhoto() {
+		sendMessage("/takePhoto");
+	}
+	void savePhoto(string filename) {
+		ofxOscMessage msgOut;
+		msgOut.setAddress("/savePhoto");
+		msgOut.addStringArg(filename);
+		oscOut.sendMessage(msgOut);
+	}
+	void draw(float x, float y) {
+		ofPushMatrix();
+		ofPushStyle();
+		ofTranslate(x, y);
+		ofFill();
+		ofSetColor(0);
+		ofRect(0, 0, width, height);
+		ofNoFill();
+		ofSetColor(255);
+		ofRect(0, 0, width, height);
+		ofLine(0, 0, width, height);
+		ofLine(width, 0, 0, height);
+		ofPopStyle();
+		ofPopMatrix();
+	}
+	bool isPhotoNew() {
+		bool prevNewPhoto = newPhoto;
+		newPhoto = false;
+		return prevNewPhoto;
+	}
+};
+
+class testApp : public ofBaseApp {
+public:
+	void setup();
+	void update();
+	void draw();
+	void generate();
+	bool nextState();
+	void keyPressed(int key);
+	
+	const static int tw = 1024 * 3;
+	const static int th = 768;
+	
+	EdsdkOsc camera;
+};
