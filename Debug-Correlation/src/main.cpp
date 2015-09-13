71282cc00e2456f889276f22a5dc2b837a69ccd9
Kyle McDonald <kyle@kylemcdonald.net>
2015-09-13 20:57:31 +0200

    better decoding, some experimental correlation work

--- Debug-Correlation/src/main.cpp
+++ Debug-Correlation/src/main.cpp
@@ -10,51 +10,86 @@ public:
     ofShortImage proMap;
     void setup() {
         ofBackground(0);
+        ofSetFrameRate(30);
         ofSetLineWidth(2);
+        loadScan("../../../SharedData/scan-1549/");
     }
     void draw() {
         if(camConfidence.isAllocated()) {
-            float totalHeight = camConfidence.getHeight() + proConfidence.getHeight();
-            float scale = ofGetHeight() / totalHeight;
-            ofScale(scale, scale);
-            ofVec2f proOffset(0, proConfidence.getHeight());
-            ofSetColor(ofColor::white);
-            proConfidence.draw(0, 0);
+            int height = ofGetHeight();
+            int width = ofGetWidth() / 2;
+            int scale = ofGetMousePressed() ? 2 : 16;
+            float cx = ofMap(mouseX, 0, ofGetWidth(), 0, proConfidence.getWidth());
+            float cy = ofMap(mouseY, 0, ofGetHeight(), 0, proConfidence.getHeight());
             
+            ofPushView();
+            ofViewport(ofRectangle(0, 0, width, height));
+            ofSetupScreenPerspective();
             ofPushMatrix();
-            ofTranslate(proOffset);
+            ofTranslate(width / 2, height / 2);
+            ofScale(scale, scale);
+            ofDrawCircle(0, 0, 4);
+            ofPushMatrix();
+            ofTranslate(-cx, -cy);
+            proConfidence.draw(0, 0);
+            ofPopMatrix();
+            ofDrawRectangle(0, 0, 1, 1);
+            ofPopMatrix();
+            ofPopView();
+            
+            ofPushView();
+            ofViewport(ofRectangle(width, 0, width, height));
+            ofSetupScreenPerspective();
+            ofPushMatrix();
+            ofScale(scale, scale);
             camConfidence.draw(0, 0);
             ofPopMatrix();
+            ofPopView();
             
-            float r = 2;
-            float drawRadius = 64;
-            ofFill();
-            ofVec2f pro = ofVec2f(mouseX, mouseY) / scale;
-            ofSeedRandom(0);
-            for(int y = -r; y <= +r; y++) {
-                for(int x = -r; x <= +r; x++) {
-                    ofVec2f cur = pro + ofVec2f(x, y);
-                    ofSetColor(ofColor::red);
-                    ofDrawRectangle(cur.x, cur.y, 1, 1);
-                    ofShortColor camColor = proMap.getColor(cur.x, cur.y);
-                    ofVec2f cam(camColor.b, camColor.g);
-                    ofSetColor(ofColor::fromHsb(ofRandom(255), 255, 255));
-                    if(cam.x > 0 && cam.y > 0) {
-                        ofVec2f center = proOffset + cam - ofVec2f(drawRadius, drawRadius) / 2;
-                        ofPushMatrix();
-                        ofTranslate(center);
-                        ofRotate(ofRandom(360));
-                        ofDrawLine(-drawRadius, 0, +drawRadius, 0);
-                        ofDrawLine(0, -drawRadius, 0, +drawRadius);
-                        ofPopMatrix();
-                    }
-                }
-            }
+//            float totalHeight = camConfidence.getHeight() + proConfidence.getHeight();
+//            float scale = ofGetHeight() / totalHeight;
+//            ofScale(scale, scale);
+//            ofVec2f proOffset(0, proConfidence.getHeight());
+//            ofSetColor(ofColor::white);
+//            proConfidence.draw(0, 0);
+//            
+//            ofPushMatrix();
+//            ofTranslate(proOffset);
+//            camConfidence.draw(0, 0);
+//            ofPopMatrix();
+//            
+//            float r = 2;
+//            float drawRadius = 64;
+//            ofFill();
+//            ofVec2f pro = ofVec2f(mouseX, mouseY) / scale;
+//            ofSeedRandom(0);
+//            for(int y = -r; y <= +r; y++) {
+//                for(int x = -r; x <= +r; x++) {
+//                    ofVec2f cur = pro + ofVec2f(x, y);
+//                    ofSetColor(ofColor::red);
+//                    ofDrawRectangle(cur.x, cur.y, 1, 1);
+//                    ofShortColor camColor = proMap.getColor(cur.x, cur.y);
+//                    ofVec2f cam(camColor.b, camColor.g);
+//                    ofSetColor(ofColor::fromHsb(ofRandom(255), 255, 255));
+//                    if(cam.x > 0 && cam.y > 0) {
+//                        ofVec2f center = proOffset + cam - ofVec2f(drawRadius, drawRadius) / 2;
+//                        ofPushMatrix();
+//                        ofTranslate(center);
+//                        ofRotate(ofRandom(360));
+//                        ofDrawLine(-drawRadius, 0, +drawRadius, 0);
+//                        ofDrawLine(0, -drawRadius, 0, +drawRadius);
+//                        ofPopMatrix();
+//                    }
+//                }
+//            }
         }
     }
     void loadScan(string path) {
+        camConfidence.getTexture().setTextureMinMagFilter(GL_NEAREST, GL_NEAREST);
+        proConfidence.getTexture().setTextureMinMagFilter(GL_NEAREST, GL_NEAREST);
         camConfidence.load(path + "/camConfidence.exr");
         proConfidence.load(path + "/proConfidence.exr");
+        proMap.setUseTexture(false);
         proMap.load(path + "/proMap.png");
     }
     void dragEvent(ofDragInfo dragInfo) {
--- SharedCode/LightLeaksUtilities.h
+++ SharedCode/LightLeaksUtilities.h
@@ -6,38 +6,38 @@ using namespace ofxCv;
 using namespace cv;
 
 void medianThreshold(cv::Mat& src, float thresholdValue) {
-	cv::Mat thresholded, filtered;
-	ofxCv::threshold(src, thresholded, thresholdValue);
-	ofxCv::medianBlur(thresholded, filtered, 3);
-	thresholded &= filtered;
-	min(src, thresholded, src);
+    cv::Mat thresholded, filtered;
+    ofxCv::threshold(src, thresholded, thresholdValue);
+    ofxCv::medianBlur(thresholded, filtered, 3);
+    thresholded &= filtered;
+    min(src, thresholded, src);
 }
 
 void buildProMap(int proWidth, int proHeight,
-								 const Mat& binaryCoded,
-								 const Mat& camConfidence,
-								 Mat& proConfidence,
-								 Mat& proMap) {	
-	int camWidth = camConfidence.cols, camHeight = camConfidence.rows;
-	ofLogVerbose() << "building proMap";
-	proMap = Mat::zeros(proHeight, proWidth, CV_16UC3);
-	proConfidence = Mat::zeros(proHeight, proWidth, CV_32FC1);
-	for(int cy = 0; cy < camHeight; cy++) {
-		for(int cx = 0; cx < camWidth; cx++) {
-			float curCamConfidence = camConfidence.at<float>(cy, cx);
-			Vec3w pxy = binaryCoded.at<Vec3w>(cy, cx);
-			unsigned short px = pxy[0], py = pxy[1];
-			if(px < proWidth && py < proHeight) {
-				Vec3w curProMap(cx, cy, 0);
-				float curProConfidence = proConfidence.at<float>(py, px);
-				if(curCamConfidence > curProConfidence) {
-					proConfidence.at<float>(py, px) = curCamConfidence;
-					proMap.at<Vec3w>(py, px) = curProMap;
-				}
-			}
-		}
-	}
-	medianThreshold(proConfidence, .25);
+                 const Mat& binaryCoded,
+                 const Mat& camConfidence,
+                 Mat& proConfidence,
+                 Mat& proMap) {
+    int camWidth = camConfidence.cols, camHeight = camConfidence.rows;
+    ofLogVerbose() << "building proMap";
+    proMap = Mat::zeros(proHeight, proWidth, CV_16UC3);
+    proConfidence = Mat::zeros(proHeight, proWidth, CV_32FC1);
+    for(int cy = 0; cy < camHeight; cy++) {
+        for(int cx = 0; cx < camWidth; cx++) {
+            float curCamConfidence = camConfidence.at<float>(cy, cx);
+            const Vec3w& pxy = binaryCoded.at<Vec3w>(cy, cx);
+            const unsigned short& px = pxy[0], py = pxy[1];
+            if(px < proWidth && py < proHeight) {
+                Vec3w curProMap(cx, cy, 0);
+                float& curProConfidence = proConfidence.at<float>(py, px);
+                if(curCamConfidence > curProConfidence) {
+                    proConfidence.at<float>(py, px) = curCamConfidence;
+                    proMap.at<Vec3w>(py, px) = curProMap;
+                }
+            }
+        }
+    }
+//    medianThreshold(proConfidence, .25);
 }
 
 void setCalibrationDataPathRoot() {
