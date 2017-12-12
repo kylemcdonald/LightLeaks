24121b4dc4ee5d2a432094f1243b4bf5363d4dca
Jonas Jongejan <jongejan@google.com>
2017-12-12 15:41:04 -0600

    Rename procamsample

--- 0-ProCamSample/addons.make
+++ 0-ProCamSample/addons.make
@@ -1,4 +1,1 @@
-ofxCv
-ofxOpenCv
-ofxOsc
 ofxWebServer
--- 0-ProCamSample/src/main.cpp
+++ 0-ProCamSample/src/main.cpp
@@ -1,217 +1,335 @@
+/*
+ This app runs the projection during the calibration. It creates multiple windows
+ and displays a coded image in each window. Each window has its own mask, which
+ might not exist on the first run. The server app generates a list of patterns to
+ be projected, and when it receives the "start" command it will iterate through them.
+ */
+
 #include "ofMain.h"
-#include "EdsdkOsc.h"
-#include "GrayCodeGenerator.h"
+#include "ofAppNoWindow.h"
+#include "ofxWebServer.h"
 
-bool primary;
-int monitorWidth;
-int totalPhysicalProjectors, totalProjectors, tw, th;
+ofJson jsonconfig;
 
-class testApp : public ofBaseApp {
+// this camera always returns after 500ms
+class FakeCamera {
+private:
+    uint64_t lastTime = 0;
+    bool newPhoto = false;
+    bool waiting = false;
+    bool startRequested = false;
+    
 public:
-	ofImage mask;
-	EdsdkOsc camera;
-    
-	GrayCodeGenerator generator;
-	bool capturing = false;
-	int totalDirection = 2;
-	int totalInverse = 2;
-	
-	// every combination of these four properties
-	int projector = 0; // n values = totalProjectors
-	int direction = 0; // false/true, 2 values
-	int inverse = 0; // false/true, 2 values
-	int pattern = 0; // depends on projector resolution
-	
-	string curDirectory;
-	long bufferTime = 100;
-	bool needToCapture = false;
-	long captureTime = 0;
-	bool referenceImage = false;
-    
-    bool generated;
-    string timestamp;
-    
-	void generate() {
-		generator.setSize(tw, th);
-		generator.setOrientation(direction == 0 ? PatternGenerator::VERTICAL : PatternGenerator::HORIZONTAL);
-		generator.setInverse(inverse == 0);
-		generator.generate();
-		stringstream dirStr;
-		dirStr <<
-        "../../../SharedData/scan-"<< timestamp << "/cameraImages/" <<
-		(direction == 0 ? "vertical/" : "horizontal/") <<
-		(inverse == 0 ? "inverse/" : "normal/");
-		curDirectory = dirStr.str();
-        ofLog() << "Creating " << curDirectory;
-		camera.createDirectory(curDirectory);
-        generated = true;
-	}
-	
-	bool nextState() {
-		pattern++;
-		if(pattern == generator.size()) {
-			pattern = 0;
-			inverse++;
-			if(inverse == totalInverse) {
-				inverse = 0;
-				direction++;
-				if(direction == totalDirection) {
-					direction = 0;
-					projector++;
-					if(projector == totalProjectors) {
-						projector = 0;
-						return false;
-					}
-				}
-			}
-			generate();
-		}
-		return true;
-	}
-	
     void setup() {
-		ofSetVerticalSync(true);
-		ofHideCursor();
-		if(ofFile::doesFileExist("../../../SharedData/mask.png")) {
-			mask.load("../../../SharedData/mask.png");
-            ofLog() << "Loaded mask";
-            ofEnableBlendMode(OF_BLENDMODE_MULTIPLY);
-            ofBackground(255);
-        } else {
-            ofEnableAlphaBlending();
-            ofBackground(0);
-        }
-        
-        // this will need to be modified if running from computer with monitor vs no monitor
-        ofSetWindowPosition(monitorWidth, 0);
-        ofSetWindowShape(tw*totalPhysicalProjectors, th);
-		ofSetLogLevel(OF_LOG_VERBOSE);
-        camera.setup();
-	}
-	
-	void update() {
-		if(!capturing) {
-			camera.update();
-            
-            if(camera.start){
-                camera.start = false;
-                
-                pattern = 0;
-                projector = 0;
-                direction = 0;
-                inverse = 0;
-                
-                dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-                    camera.takePhoto(curDirectory + ofToString(pattern) + ".jpg", primary);
-                });
-                
-                timestamp = ofToString(ofGetHours())+ofToString(ofGetMinutes());
-                generate();
-
-                capturing = true;
-            }
-		}
-		if(camera.isPhotoNew()) {
-            if(camera.error){
-                captureTime = ofGetElapsedTimeMillis();
-                needToCapture = true;
-            } else {
-                if(nextState()) {
-                    captureTime = ofGetElapsedTimeMillis();
-                    needToCapture = true;
-                } else {
-                    cout<<"Done taking photos. Hurray!"<<endl;
-                    capturing = false;
-                }
-            }
-            
-		}
-	}
-	
-    void draw() {
-        int singleWidth = tw / totalPhysicalProjectors;
-        
-        if(!primary) {
-            float offset = (totalPhysicalProjectors / 2) * singleWidth;
-            ofTranslate(-offset, 0);
-        }
-        
-		ofSetColor(255);
-       
-		if(!capturing) {
-
-			/*ofPushMatrix();
-			ofScale(.25, .25);
-			camera.draw(0, 0);
-			ofPopMatrix();*/
-            
-            for(int i = 0; i < totalPhysicalProjectors; i++) {
-                float hue = ofMap(i, 0, totalPhysicalProjectors, 0, 255);
-                ofSetColor(ofColor::fromHsb(hue, 255, 255));
-                ofDrawRectangle(i * singleWidth, 0, singleWidth, th);
-            }
-        } else {
-            if(generated){
-                generator.get(pattern).draw(projector * tw, 0);
-            }
-            if(mask.getWidth() > 0) {
-                mask.draw(0, 0);
+        ofAddListener(ofEvents().update, this, &FakeCamera::updateInternal);
+    }
+    void updateInternal(ofEventArgs &args) {
+        uint64_t curTime = ofGetElapsedTimeMillis();
+        uint64_t timeDiff = 500;
+        if (waiting) {
+            if(curTime > lastTime + timeDiff) {
+                newPhoto = true;
+                waiting = false;
             }
         }
-		if(needToCapture && (ofGetElapsedTimeMillis() - captureTime) > bufferTime) {
-            camera.takePhoto(curDirectory + ofToString(pattern) + ".jpg", primary);
-			needToCapture = false;
-		}
-	}
-	
-	void keyPressed(int key) {
-		if(key == OF_KEY_RIGHT) {
-			pattern++;
-		}
-        if(key == OF_KEY_LEFT) {
-			pattern--;
-		}
-		if(key == ' ') {
-            timestamp = ofToString(ofGetHours())+ofToString(ofGetMinutes());
-            generate();
-            
-			camera.takePhoto(curDirectory + ofToString(pattern) + ".jpg", primary);
-			capturing = true;
-		}
-		if(key == 'r') {
-			camera.takePhoto(curDirectory + ofToString(pattern) + ".jpg", primary);
-			referenceImage = true;
-		}
-		if(key == 'f') {
-			ofToggleFullscreen();
-		}
-	}
+    }
+    void takePhoto(string filename) {
+        cout << filename << endl;
+        lastTime = ofGetElapsedTimeMillis();
+        waiting = true;
+    }
+    bool isStartRequested() {
+        bool prevStartRequested = startRequested;
+        startRequested = false;
+        return prevStartRequested;
+    }
+    bool isPhotoNew() {
+        bool prevNewPhoto = newPhoto;
+        newPhoto = false;
+        return prevNewPhoto;
+    }
+    
+    // override the start request using this projection app
+    void fakeStart() {
+        startRequested = true;
+    }
 };
 
-int main() {
-	ofXml settings;
-	settings.load("../../../SharedData/settings.xml");
-	totalProjectors = settings.getIntValue("projectors/count");
-    totalPhysicalProjectors = totalProjectors;
-	tw = settings.getIntValue("projectors/width");
-	th = settings.getIntValue("projectors/height");
+
+// this camera is connected on network
+class NetworkCamera {
+private:
+    bool newPhoto = false;
+    bool startRequested = false;
     
-    monitorWidth = settings.getIntValue("monitorWidth");
-    primary = settings.getBoolValue("primary");
-    cout << "running as primary: " << primary << endl;
-    
-    if(settings.getBoolValue("sample/singlepass")) {
-        ofLog() << "Using single pass sampling";
-        tw *= totalProjectors;
-        totalProjectors = 1;
+public:
+    void setup() {
     }
-	
-    ofGLFWWindowSettings win;
     
-    win.width = 1024;
-    win.height = 768;
-    win.multiMonitorFullScreen = true;
+    void takePhoto(string filename) {
+        ofLog() <<"Take photo"<< filename;
+        ofStringReplace(filename, "../../../SharedData", "/SharedData");
+        string hostname = jsonconfig["osc"]["camera"];
+        auto resp = ofLoadURL("http://"+hostname+":8080/actions/takePhoto/"+filename);
+        if(resp.status != 200){
+            ofLogError()<<"Could not trigger camera "<<resp.error<< " Status: "<<resp.status;
+        } else {
+            newPhoto = true;
+        }
+    }
+    bool isStartRequested() {
+        bool prevStartRequested = startRequested;
+        startRequested = false;
+        return prevStartRequested;
+    }
+    bool isPhotoNew() {
+        bool prevNewPhoto = newPhoto;
+        newPhoto = false;
+        return prevNewPhoto;
+    }
     
-    ofCreateWindow(win)->setFullscreen(true);
-    ofRunApp(new testApp());
+    // override the start request using this projection app
+    void fakeStart() {
+        startRequested = true;
+    }
+};
+
+
+
+class ServerApp : public ofBaseApp, public ofxWSRequestHandler {
+public:
+    bool debug = false;
+    bool capturing = false;
+    bool needToCapture = false;
+    uint64_t bufferTime = 100;
+    uint64_t lastCaptureTime = 0;
+    string timestamp;
+    int pattern = 0;
+    vector<tuple<int,int,int>> patterns;
+    NetworkCamera camera;
+    ofxWebServer webserver;
+
+    void setup() {
+        ofLog() << "Running";
+        camera.setup();
+        webserver.start("httpdocs", 8000);
+        webserver.addHandler(this, "actions/*");
+
+    }
+    void config(ofVec2f box) {
+        int xk = ceil(log2(box.x));
+        int yk = ceil(log2(box.y));
+        
+        int axis, level, inverted;
+        axis = 0;
+        for(level = 0; level < xk; level++) {
+            for(int inverted : {0,1}) {
+                patterns.emplace_back(make_tuple(axis, level, inverted));
+            }
+        }
+        axis = 1;
+        for(level = 0; level < yk; level++) {
+            for(int inverted : {0,1}) {
+                patterns.emplace_back(make_tuple(axis, level, inverted));
+            }
+        }
+        
+        cout << "Bounding box: " << box << endl;
+        cout << "level count: " << xk << ", " << yk << endl;
+        cout << "List of patterns:" << endl;
+        for(auto cur : patterns) {
+            tie(axis, level, inverted) = cur;
+            cout << "\t" << axis << ", " << level << ", " << inverted << endl;
+        }
+    }
+    bool nextState() {
+        pattern++;
+        if (pattern == patterns.size()) {
+            pattern = 0;
+            return false;
+        }
+        return true;
+    }
+    void update() {
+        uint64_t curTime = ofGetElapsedTimeMillis();
+        if(!capturing) {
+            if(camera.isStartRequested()) {
+                timestamp = ofToString(ofGetHours(),2,'0') + ofToString(ofGetMinutes(),2,'0');
+                capturing = true;
+                needToCapture = true;
+            }
+        }
+        if(capturing && camera.isPhotoNew()) {
+            if(nextState()) {
+                lastCaptureTime = curTime;
+                needToCapture = true;
+            } else {
+                cout << "Done taking photos. Hurray!" << endl;
+                capturing = false;
+            }
+        }
+        if(capturing) {
+            if(needToCapture && curTime > bufferTime + lastCaptureTime) {
+                string directory = "../../../SharedData/scan-" + timestamp + "/cameraImages/" +
+                    (getAxis() == 0 ? "vertical/" : "horizontal/") +
+                    (getInverted() == 0 ? "normal/" : "inverse/");
+                camera.takePhoto(directory + ofToString(getLevel()) + ".jpg");
+                needToCapture = false;
+            }
+        }
+    }
+    void keyPressed(int key) {
+        if(key == 'd') {
+            debug = !debug;
+        }
+        if(key == 's') {
+            camera.fakeStart();
+        }
+    }
+    bool getDebug() {
+        return debug;
+    }
+    int getAxis() {
+        return get<0>(patterns[pattern]);
+    }
+    int getLevel() {
+        return get<1>(patterns[pattern]);
+    }
+    int getInverted() {
+        return get<2>(patterns[pattern]);
+    }
+    
+    void start(){
+        pattern = 0;
+        camera.fakeStart();
+    }
+    
+    void httpGet(string url){
+        if(ofIsStringInString(url,"/actions/start") == 1){
+            start();
+            httpResponse("Started");
+        }
+        if(ofIsStringInString(url,"/actions/stop") == 1){
+            capturing = false;
+            needToCapture = false;
+            httpResponse("Stopped");
+        }
+        if(ofIsStringInString(url,"/actions/cameraHostname") == 1){
+            string hostname = jsonconfig["osc"]["camera"];
+            httpResponse(hostname);
+        }
+        if(ofIsStringInString(url,"/actions/currentPattern") == 1){
+            httpResponse(ofToString(pattern)+"/"+ofToString(patterns.size()));
+        }
+
+    }
+};
+
+class ClientApp : public ofBaseApp {
+public:
+    int id, xcode, ycode;
+    float hue;
+    shared_ptr<ServerApp> server;
+    
+    ofShader shader;
+    ofImage mask;
+
+    void config(int id, int n, int xcode, int ycode, shared_ptr<ServerApp> server) {
+        this->id = id;
+        this->hue = id / float(n);
+        this->xcode = xcode;
+        this->ycode = ycode;
+        this->server = server;
+        
+        string maskFn = "mask-" + ofToString(id) + ".png";
+        if(ofFile::doesFileExist(maskFn)) {
+            if(mask.load(maskFn)) {
+                cout << "Loaded mask: " << maskFn << endl;
+            } else {
+                cout << "Error loading mask: " << maskFn << endl;
+            }
+        } else {
+            cout << "Mask does not exist: " << maskFn << endl;
+        }
+    }
+    
+    void setup() {
+        ofBackground(0);
+        ofSetVerticalSync(true);
+        ofHideCursor();
+        ofDisableAntiAliasing();
+        shader.load("shader");
+    }
+    void update() {
+    }
+    void draw() {
+        shader.begin();
+        shader.setUniform1i("height", ofGetHeight());
+        shader.setUniform1i("axis", server->getAxis());
+        shader.setUniform1i("level", server->getLevel());
+        shader.setUniform1i("inverted", server->getInverted());
+        shader.setUniform1i("xcode", xcode);
+        shader.setUniform1i("ycode", ycode);
+        ofDrawRectangle(0, 0, ofGetWidth(), ofGetHeight());
+        shader.end();
+        
+        ofPushStyle();
+        if(server->getDebug()) {
+            ofSetColor(ofColor_<float>::fromHsb(hue, 1, 1));
+            ofDrawRectangle(0, 0, ofGetWidth(), ofGetHeight());
+            string fps = ofToString(int(round(ofGetFrameRate())));
+            ofDrawBitmapStringHighlight(ofToString(id) + "/" + fps, 10, 20);
+        } else {
+            ofEnableBlendMode(OF_BLENDMODE_MULTIPLY);
+            if(mask.isAllocated())
+                mask.draw(0, 0);
+        }
+        ofPopStyle();
+    }
+    void keyPressed(int key) {
+        server->keyPressed(key);
+    }
+   
+};
+
+ofVec2f getBoundingBox(const ofJson& projectors) {
+    ofVec2f box;
+    for(auto projector : projectors) {
+        int x = int(projector["xcode"]) + int(projector["width"]);
+        int y = int(projector["ycode"]) + int(projector["height"]);
+        box.x = MAX(box.x, x);
+        box.y = MAX(box.y, y);
+    }
+    return box;
+}
+
+int main() {
+    jsonconfig = ofLoadJson("../../../SharedData/settings.json");
+    
+    shared_ptr<ofAppNoWindow> winServer(new ofAppNoWindow);
+    shared_ptr<ServerApp> appServer(new ServerApp);
+    ofVec2f box = getBoundingBox(jsonconfig["projectors"]);
+    appServer->config(box);
+    ofRunApp(winServer, appServer);
+    
+    ofGLFWWindowSettings settings;
+    settings.setGLVersion(3,2);
+    settings.decorated = false;
+    settings.numSamples = 1;
+    settings.resizable = true;
+    
+    int n = jsonconfig["projectors"].size();
+    for(int i = 0; i < n; i++) {
+        ofJson curConfig = jsonconfig["projectors"][i];
+        settings.monitor = curConfig["monitor"];
+        settings.width = curConfig["width"];
+        settings.height = curConfig["height"];
+        settings.setPosition(ofVec2f(curConfig["xwindow"], curConfig["ywindow"]));
+        shared_ptr<ofAppBaseWindow> winClient = ofCreateWindow(settings);
+        shared_ptr<ClientApp> appClient(new ClientApp);
+        appClient->config(i, n, curConfig["xcode"], curConfig["ycode"], appServer);
+        ofRunApp(winClient, appClient);
+    }
+    
+    ofRunMainLoop();
 }
