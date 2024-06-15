0a722af07aad15b9e933e8ac8035ec7cd4e02715
Jonas Jongejan <jonas@picturestudio.ai>
2024-06-15 08:34:09 +0200

    Transfer

--- camamokjs/src/transferFromSd.js
+++ camamokjs/src/transferFromSd.js
@@ -5,13 +5,13 @@ const data = require("../../SharedData/_scheduledDownload.json");
 const fs = require("fs");
 const sdPath = "/Volumes/LEXAR/DCIM/";
 
-console.log(`Downloading ${data.length} files...`);
+console.log(`Transfering ${data.length} files...`);
 
 for (const file of data) {
   const source = `${sdPath}${file.url.split("/sd/")[1]}`;
   const dst = `../SharedData/${file.dest}`;
 
-  console.log(`Downloading ${source} to ${dst}`);
+  console.log(`Transfering ${source} to ${dst}`);
 
   fs.mkdirSync(dst.split("/").slice(0, -1).join("/"), { recursive: true });
   fs.copyFileSync(source, dst);
